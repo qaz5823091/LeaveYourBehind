fa8114d136dd1ae32bb743acd809c72e8285365c
qaz5823091 <r19991106@gmail.com>
2022-06-13 17:00:53 +0800

    edit server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ var express = require('express');
 var app = express();
 
 // mount on port 3000
-var server = app.listen(3000);
+const PORT = process.env.PORT || 3000;
+var server = app.listen(PORT);
 
 app.use(express.static('public'));
 
